79f89af2c6e1572a4e82172e2c30e9062f32e901
Inna Kostich <114675831+innakostich@users.noreply.github.com>
2023-05-30 15:12:24 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,31 +1,13 @@
-
-
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
-// import { BrowserRouter, Route } from 'react-router-dom';
-// import App from './components/App';
-// import './index.css';
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <BrowserRouter>
-//       <Route component={App} />
-//     </BrowserRouter>
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
 import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import ReactDOM from 'react-dom/client';
 import App from './components/App';
 import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 
-createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-      </Routes>
+    <BrowserRouter basename="/goit-react-hw-05-movies/">
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
