bd3793686b84f0d25b1f2d7232d565240e49fc24
Inna Kostich <114675831+innakostich@users.noreply.github.com>
2023-06-01 12:14:40 +0200

    Update Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,14 +1,60 @@
+// import React, { useState, useEffect } from 'react';
+// import { fetchTrendingMovies } from 'services/movies.api';
+// import { BeatLoader } from 'react-spinners';
+
+
+// const Home = () => {
+//   const [trendingMovies, setTrendingMovies] = useState([]);
+//   const [loading, setLoading] = useState(true);
+
+//   useEffect(() => {
+//     const fetchTrendingMovies = async () => {
+//       try {
+//         const response = await fetchTrendingMovies();
+//         setTrendingMovies(response.results);
+//         setLoading(false);
+//       } catch (error) {
+//         console.error('Failed to fetch trending movies:', error);
+//         setLoading(false);
+//       }
+//     };
+
+//     fetchTrendingMovies();
+//   }, []);
+
+//   if (loading) {
+//     return (
+//       <div className={css.loaderContainer}>
+//         <BeatLoader color="#555" />
+//       </div>
+//     );
+//   }
+
+//   return (
+//     <div className={css.home}>
+//       <h1>Trending Movies</h1>
+//       <ul>
+//         {trendingMovies.map((movie) => (
+//           <li key={movie.id}>{movie.title}</li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// };
+
+// export default Home;
+
+
 import React, { useState, useEffect } from 'react';
 import { fetchTrendingMovies } from 'services/movies.api';
 import { BeatLoader } from 'react-spinners';
-
 
 const Home = () => {
   const [trendingMovies, setTrendingMovies] = useState([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const fetchTrendingMovies = async () => {
+    const fetchMovies = async () => {
       try {
         const response = await fetchTrendingMovies();
         setTrendingMovies(response.results);
@@ -19,19 +65,19 @@ const Home = () => {
       }
     };
 
-    fetchTrendingMovies();
+    fetchMovies();
   }, []);
 
   if (loading) {
     return (
-      <div className={css.loaderContainer}>
+      <div style={{ display: 'flex', justifyContent: 'center', marginTop: '2rem' }}>
         <BeatLoader color="#555" />
       </div>
     );
   }
 
   return (
-    <div className={css.home}>
+    <div>
       <h1>Trending Movies</h1>
       <ul>
         {trendingMovies.map((movie) => (
